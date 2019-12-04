1cef3a3a7849c7fe55c0e8456cd7262e649eb1b5
Andrew Dinh <adinh@sightmachine.com>
2019-12-04 00:26:16 -0800

    Add function to create an event

--- src/github.com/andrewtdinh/go-rest-api/main.go
+++ src/github.com/andrewtdinh/go-rest-api/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -26,6 +28,20 @@ var events = allEvents{
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome home!")
+}
+
+func createEvent(w http.ResponseWriter, r *http.Request) {
+	var newEvent event
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+
+	json.Unmarshal(reqBody, &newEvent)
+	events = append(events, newEvent)
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(newEvent)
 }
 
 func main() {
