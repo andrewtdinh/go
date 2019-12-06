a6d9a0479b7b769d6d131118cfcfa9ad21352f64
Andrew Dinh <adinh@sightmachine.com>
2019-12-06 09:56:55 -0800

    Add method to return all requests

--- src/github.com/andrewtdinh/go-buoyant/main.go
+++ src/github.com/andrewtdinh/go-buoyant/main.go
@@ -16,11 +16,7 @@ type request struct {
 
 type allRequests []request
 
-var requests = allRequests{
-	{
-		Endpoint: "/",
-	},
-}
+var requests = allRequests{}
 
 func homeLink(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "/")
@@ -40,9 +36,24 @@ func printEndpoint(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(newEvent)
 }
 
+func getAllRequests(w http.ResponseWriter, r *http.Request) {
+	var newRequest request
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Kindly enter data with the event title and description only in order to update")
+	}
+
+	json.Unmarshal(reqBody, &newEvent)
+	events = append(events, newEvent)
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(newEvent)
+}
+
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", homeLink)
+	router.HandleFunc("/requests", getAllRequests)
 	router.HandleFunc("/books", printEndpoint).Methods("GET")
 	router.HandleFunc("/books/{id: [0-9]+}", printEndpoint).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8082", router))
